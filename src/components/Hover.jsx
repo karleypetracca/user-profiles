a42fcfeab937c395bb235b69fe8fada74275b108
Karley Petracca <karleypetracca@gmail.com>
2020-03-27 15:47:07 -0400

    cleaned up visual for hover

--- src/components/Hover.jsx
+++ src/components/Hover.jsx
@@ -20,18 +20,28 @@ class Hover extends Component {
 	render() {
         let hoverStyle = {}, divStyle = {};
         if (this.state.hover) {
-            divStyle = { cursor: "pointer" };
-            hoverStyle = { visibility: "visible", cursor: "pointer" }
+            divStyle = {
+                cursor: "pointer",
+                display: "flex",
+                alignItems: "center",
+                margin: "20px 0"
+            };
+            hoverStyle = { visibility: "visible"}
         } else {
-            hoverStyle = { visibility: "collapse" };
+            divStyle = {
+				display: "flex",
+				alignItems: "center",
+				margin: "20px 0"
+			};
+            hoverStyle = { visibility: "collapse"};
         }
 		return (
 			<div
-				style={{ display: "block" }} style={divStyle}
+				style={divStyle}
 				onMouseEnter={this.toggleHover.bind(this)}
 				onMouseLeave={this.toggleHover.bind(this)}
 			>
-				<Icon className={this.props.iconClass} style={divStyle} />
+				<Icon className={this.props.iconClass} style={{ marginRight: "8px"}}/>
 				<p style={hoverStyle}>
 					<b>{this.props.title}:</b> {this.props.text}
 				</p>
